46ec5c45c01f790cbb649403dbede3bb7b8ea265
Oluchi.Jonathan <oluchi.jonathan@hrsdc-rhdcc.gc.ca>
2022-01-21 18:44:20 +0000

    updating

--- myaliases.sh
+++ myaliases.sh
@@ -2,4 +2,8 @@ echo "loading aliases...."
 #my aliases list 
 alias gst="git status"
 alias gl="git log --oneline"
+alias hey="echo hey there"
+alias gst="git status"
+alias reload="source ~/.bashrc"
+alias editbashrc="code ~/.bashrc"
 # I am a comment
